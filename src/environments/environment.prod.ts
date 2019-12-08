10cf12b55690aa63acd8f4f236a709ea7a90e2af
Dmytro Novikov <nodm.pe@gmail.com>
2019-12-08 11:16:50 +0200

    + add version and build number to env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
+  version: '%PACKAGE_VERSION%',
+  buildNumber: '%$TRAVIS_BUILD_NUMBER%',
   darkSkyApiKey: '%DARK_SKY_API_KEY%',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,8 @@
 
 export const environment = {
   production: false,
+  version: '%PACKAGE_VERSION%',
+  buildNumber: '%$TRAVIS_BUILD_NUMBER%',
   darkSkyApiKey: '%DARK_SKY_API_KEY%',
 };
 
