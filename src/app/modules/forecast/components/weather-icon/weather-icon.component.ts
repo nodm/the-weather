1a00efa78fe628122121b861518780ed83691200
Dmytro Novikov <nodm.pe@gmail.com>
2019-12-09 15:31:03 +0200

    * POC about icons issue

--- src/app/modules/forecast/components/weather-icon/weather-icon.component.ts
+++ src/app/modules/forecast/components/weather-icon/weather-icon.component.ts
@@ -4,12 +4,17 @@ import { WeatherSummary } from '../../models/weather-summary.type';
 
 @Component({
   selector: 'app-weather-icon',
-  template: '<div [class]="classes"></div>',
+  // template: '<div [class]="classes"></div>',
+  template: '<img class="icon" [src]="imageSource" alt="">',
   styleUrls: ['./weather-icon.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WeatherIconComponent {
   @Input() name: WeatherSummary;
+
+  public get imageSource(): string {
+    return `assets/images/${this.name}.svg`;
+  }
 
   public get classes(): string {
     return `icon icon--${this.name}`;
