35f0c5b1b16c7a4bac2f630358375508c413f8e3
Dmytro Novikov <nodm.pe@gmail.com>
2019-12-17 23:42:33 +0200

    ! fix deployment error

--- src/app/shared/services/app-config.service.ts
+++ src/app/shared/services/app-config.service.ts
@@ -12,10 +12,15 @@ export class AppConfigService {
   }
 
   public get darkSkyApiKey(): string {
-    return this.config.darkSkyApiKey === '%DARK_SKY_API_KEY%' ? '<YOUR_DARK_SKY_API_KEY>' : this.config.darkSkyApiKey;
+    // return this.config.darkSkyApiKey === '%DARK_SKY_API_KEY%' ? '<YOUR_DARK_SKY_API_KEY>' : this.config.darkSkyApiKey;
+    return this.config.darkSkyApiKey === '%DARK_SKY_API_KEY%' ? '59cd2c19bac276c0e4555230f327e3ee' : this.config.darkSkyApiKey;
+
   }
 
   public get vapidPublicKey(): string {
-    return this.config.vapidPublicKey === '%VAPID_PUBLIC_KEY%' ? '<YOUR_VAPID_PUBLIC_KEY>' : this.config.vapidPublicKey;
+    return this.config.vapidPublicKey === '%VAPID_PUBLIC_KEY%'
+      ? 'BF7qv2jYgcI0XTRzFPYZGLok95M9geyg2DpHIc-9xXnMcyI_KDplNRiDFVJS4Q189OYbB3wSkPTqp8I2eV80b80'
+      // ? '<YOUR_VAPID_PUBLIC_KEY>'
+      : this.config.vapidPublicKey;
   }
 }
